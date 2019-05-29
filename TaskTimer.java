9af90a4cafdd38be455eff4a112281eb2cdf9ff8
Alisha Gomes <41697828+alishagomes@users.noreply.github.com>
2019-05-29 19:20:25 -0400

    Update TaskTimer.java

--- TaskTimer.java
+++ TaskTimer.java
@@ -8,12 +8,23 @@ public class TaskTimer
 	public static Timer timer;
 
 public static void main(String[] args) {
-   
-
-	
+ 
+    System.out.println("Welcome to: ");
+    System.out.println("");
+	System.out.println("  ______           __      _______                        \n" + 
+			" /_  __/___ ______/ /__   /_  __(_)___ ___  ___  _____    \n" + 
+			"  / / / __ `/ ___/ //_/    / / / / __ `__ \\/ _ \\/ ___/    \n" + 
+			" / / / /_/ (__  ) ,<      / / / / / / / / /  __/ /        \n" + 
+			"/_/  \\__,_/____/_/|_|    /_/ /_/_/ /_/ /_/\\___/_/         \n" + 
+			"                                                          ");
 	Scanner scan2 = new Scanner(System.in);
     System.out.print("How many seconds do you want to do this task: ");
     String sec = scan2.nextLine();
+    if (sec.equals("exit"))
+    {
+    	MainClass.main(null);
+    }
+    
     int delaySeconds= 1000;
     int total = 1000;
     timer = new Timer();
@@ -23,7 +34,9 @@ public static void main(String[] args) {
 
         public void run() 
         {
+        	
             System.out.println(setInterval());
+        	
 
         }
     }, delaySeconds, total);
@@ -33,10 +46,16 @@ private static final int setInterval() {
     if (howLong == 1)
     {
     	timer.cancel();
-    	System.out.println("STOP TASK NOW");
+    	System.out.println("███████╗████████╗ ██████╗ ██████╗     \n" + 
+    			"██╔════╝╚══██╔══╝██╔═══██╗██╔══██╗    \n" + 
+    			"███████╗   ██║   ██║   ██║██████╔╝    \n" + 
+    			"╚════██║   ██║   ██║   ██║██╔═══╝     \n" + 
+    			"███████║   ██║   ╚██████╔╝██║         \n" + 
+    			"╚══════╝   ╚═╝    ╚═════╝ ╚═╝         \n" + 
+    			"                                      \n" + 
+    			"");
     }
 
-    return -- howLong ;
-
+    return -- howLong ;    
 }
 }
