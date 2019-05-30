c3b8e014986c465197d7b47f612221d7c4be22f9
Alisha Gomes <41697828+alishagomes@users.noreply.github.com>
2019-05-30 17:24:52 -0400

    Create Notes.java

---
+++ Notes.java
@@ -0,0 +1,16 @@
+public class Notes 
+{
+
+	public static void main (String args[])
+	{
+		System.out.println("");
+		System.out.println("    _   __      __           \n" + 
+				"   / | / /___  / /____  _____\n" + 
+				"  /  |/ / __ \\/ __/ _ \\/ ___/\n" + 
+				" / /|  / /_/ / /_/  __(__  ) \n" + 
+				"/_/ |_/\\____/\\__/\\___/____/  \n" + 
+				"                             ");
+	}
+	
+	
+}
